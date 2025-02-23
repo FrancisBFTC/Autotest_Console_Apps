830d7682147e84f427b1c1acda18937dff6f10e9
FrancisBFTC <bftcorporations@gmail.com>
2025-02-23 15:17:20 -0300

    fix/feat: Corrige erros de alocação e adiciona novos comandos

--- src/tester.c
+++ src/tester.c
@@ -17,6 +17,8 @@ char* output = NULL;
 char* setup = NULL;
 char* success_test = NULL;
 char* error_test = NULL;
+char* setup_test = NULL;
+char* name = NULL;
 
 int linenum = 1;
 
@@ -99,6 +101,50 @@ char* read_file(const char* filename){
 	return datas;
 }
 
+/*
+char* testname = NULL;
+char* input = NULL;
+char* output = NULL;
+char* setup = NULL;
+char* success_test = NULL;
+char* error_test = NULL;
+char* setup_test = NULL;
+char* name = NULL;
+*/
+void reset_all(){
+	if(name != NULL)
+		free(name);
+	if(success != NULL)
+		free(success);
+	if(error != NULL)
+		free(error);
+	if(setup != NULL)
+		free(setup);
+	if(testname != NULL)
+		free(testname);
+	if(input != NULL)
+		free(input);
+	if(output != NULL)
+		free(output);
+	if(success_test != NULL)
+		free(success_test);
+	if(error_test != NULL)
+		free(error_test);
+	if(setup_test != NULL)
+		free(setup_test);
+				
+	success = NULL;
+	error = NULL;
+	setup = NULL;
+	name = NULL;
+	testname = NULL;
+	input = NULL;
+	output = NULL;
+	success_test = NULL;
+	error_test = NULL;
+	setup_test = NULL;
+}
+
 void change_color(int textColor, int backColor){
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, (backColor << 4) | textColor);
@@ -115,8 +161,10 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
     isTest = false;
     bool isSuccessMsg = false;
     bool isErrorMsg = false;
+    bool isNameMsg = false;
     bool isSuccessLocal = false;
     bool isErrorLocal = false;
+    bool isSetupLocal = false;
     bool isInputMsg = false;
     bool isOutputMsg = false;
     bool isSetupMsg = false;
@@ -124,9 +172,6 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
     int success_count = 0;
     int fail_count = 0;
     int test_count = 0;
-    
-    change_color(11, 0);
-    printf("Processing Tests...\n");
     
     while (fgets(line, sizeof(line), file)) {
     	format_line();
@@ -138,11 +183,13 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 		int len = 0;
 		bool isSuccessTmp = false;
 		bool isErrorTmp = false;
+		bool isNameTmp = false;
 		bool isInputTmp = false;
 		bool isOutputTmp = false;
 		bool isSetupTmp = false;
 		bool isSuccessTest = false;
 		bool isErrorTest = false;
+		bool isSetupTest = false;
 		bool isConcat = true;
 		
 		while (token != NULL) {
@@ -152,10 +199,22 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 			bool isInputSymbol = strcmp(token, "INPUT=") == 0;
 			bool isOutputSymbol = strcmp(token, "OUTPUT=") == 0;
 			bool isSetupSymbol = strcmp(token, "SETUP=") == 0;
+			bool isNameSymbol = strcmp(token, "NAME=") == 0;
 			
 			// TODO: Corrigir error de Token Success e Error com caracteres tudo junto
 			if(strcmp(token, "MESSAGE") == 0 || isMessageSymbol){
 				if(isMessageSymbol || linetmp[strcspn(&linetmp[0], "{")] == '{'){
+					reset_all();
+					
+					isSuccessMsg = false;
+				    isErrorMsg = false;
+				    isNameMsg = false;
+				    isSuccessLocal = false;
+				    isErrorLocal = false;
+				    isSetupLocal = false;
+				    isInputMsg = false;
+				    isOutputMsg = false;
+				    isSetupMsg = false;
 					isMessage = true;
 					break;
 				}else{
@@ -178,6 +237,16 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 						return false;
 					}
 					
+			}else if(strcmp(token, "NAME") == 0 || isNameSymbol){
+					index = strcspn(&linetmp[0], "=");
+					len = strlen(&linetmp[index + 1]);
+					if((isNameSymbol || linetmp[index] == '=') && isMessage){
+						isNameMsg = true;
+						isNameTmp = isNameMsg;
+					}else{
+						print_msg_error();
+						return false;
+					}
 			}else if(strcmp(token, "SUCCESS") == 0 || isSuccessSymbol){
 					index = strcspn(&linetmp[0], "=");
 					len = strlen(&linetmp[index + 1]);
@@ -231,18 +300,33 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 			}else if(strcmp(token, "SETUP") == 0 || isSetupSymbol){
 					index = strcspn(&linetmp[0], "=");
 					len = strlen(&linetmp[index + 1]);
-					if((isSetupSymbol || linetmp[index] == '=') && isTest){
-						isSetupMsg = true;
-						isSetupTmp = isSetupMsg;
+					if((isSetupSymbol || linetmp[index] == '=') && (isMessage || isTest)){
+						if(!isTest){
+							isSetupMsg = true;
+							isSetupTmp = isSetupMsg;	
+						}else{
+							isSetupLocal = true;
+							isSetupTest = isSetupLocal;
+						}
+						
 					}else{
 						print_test_error();
 						return false;
 					}
 			}else{
 				if(token[0] == '}'){
-					if(isMessage)
+					if(isMessage){
 						isMessage = false;
-					else if(isTest){
+						change_color(11, 0);
+    					if(isNameMsg)
+    						printf("Processing '%s'...\n", name);
+    					else
+    						printf("Processing Tests...\n");
+    					if(name != NULL){
+    						free(name);
+    						name = NULL;
+						}
+					}else if(isTest){
 						
 						change_color(12, 0);
 						printf("\ttest ");
@@ -253,10 +337,15 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 						
 						save_file("input.dat", input);
 						
-						const char* data = " input.dat > output.dat";
-						setup = (char*) realloc(setup, strlen(setup) + strlen(data) + 2);
-						strcat(setup, data);
-						system(setup);
+						if(isSetupLocal){
+							const char* data = " input.dat > output.dat";
+							setup_test = (char*) realloc(setup_test, strlen(setup_test) + strlen(data) + 2);
+							strcat(setup_test, data);
+							system(setup_test);
+						}else{
+							system(setup);
+						}
+		
 						char* content = read_file("output.dat");
 						
 						if(strcmp(content, output) == 0){
@@ -304,7 +393,15 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 							if(isErrorLocal)
 								printf(" %s\n", error_test);
 							else
-								printf(" %s\n", error);	
+								printf(" %s\n", error);
+								
+							change_color(14, 0);
+							printf("\t\tSetup: ");
+							change_color(7, 0);
+							if(isSetupLocal)
+								printf(" %s\n", setup_test);
+							else
+								printf(" %s\n", setup);	
 						}
 						
 						change_color(7, 0);
@@ -321,9 +418,10 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 							isErrorLocal = false;
 						}
 						
-						if(setup != NULL){
-							free(setup);
-							setup = NULL;
+						if(isSetupLocal){
+							free(setup_test);
+							setup_test = NULL;
+							isSetupLocal = false;
 						}
 							
 						isConcatInput = false;
@@ -339,7 +437,7 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 				}	
 			} 
 			
-			if(isSuccessTmp || isErrorTmp || isInputTmp || isOutputTmp || isSuccessTest || isErrorTest || isSetupTmp){
+			if(isSuccessTmp || isErrorTmp || isNameTmp || isInputTmp || isOutputTmp || isSuccessTest || isErrorTest || isSetupTmp || isSetupTest){
 				
 				int posQuote = 0;
 				int counter = 0;
@@ -389,9 +487,27 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 						}else if(isErrorTmp){
 							if(error != NULL && !counter)
 								free(error);
-							error = (error == NULL) ? (char*) malloc(posQuote + 1) : (char*) realloc(error, posQuote);
-							strcpy(error, buffer);
+							error = (error == NULL) ? (char*) malloc(posQuote + 1) : (char*) realloc(error, posQuote + 1);
+							if(!counter)
+								strcpy(error, buffer);
+							else{
+								strcat(error, "\n");
+								strcat(error, buffer);
+							}
 							error[posQuote] = '\0';
+							posQuote += 2;
+						}else if(isNameTmp){
+							if(name != NULL && !counter)
+								free(name);
+							name = (name == NULL) ? (char*) malloc(posQuote + 1) : (char*) realloc(name, posQuote + 1);
+							if(!counter)
+								strcpy(name, buffer);
+							else{
+								strcat(name, "\n");
+								strcat(name, buffer);
+							}
+							name[posQuote] = '\0';
+							posQuote += 2;
 						}else if(isInputTmp){
 							if(input != NULL && !counter)
 								free(input);
@@ -441,9 +557,19 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 						}else if(isSetupTmp){
 							if(setup != NULL)
 								free(setup);
+							
 							setup = (char*) malloc(posQuote + 1);
 							strcpy(setup, buffer);
 							setup[posQuote] = '\0';
+							const char* data = " input.dat > output.dat";
+							setup = (char*) realloc(setup, strlen(setup) + strlen(data) + 2);
+							strcat(setup, data);
+						}else if(isSetupTest){
+							if(setup_test != NULL)
+								free(setup_test);
+							setup_test = (char*) malloc(posQuote + 1);
+							strcpy(setup_test, buffer);
+							setup_test[posQuote] = '\0';
 						}
 						if(isConcat){
 							fgets(line, sizeof(line), file);
@@ -472,44 +598,50 @@ bool tester(const char* filename, bool verbose, int* scount, int* fcount, int* t
 	*scount = success_count;
 	*fcount = fail_count;
 	*tests = test_count;
-	if(success != NULL)
-		free(success);
-	if(error != NULL)
-		free(error);
+	
+	reset_all();
 		
+	fclose(file);
 	return true;
 }
 
+void show_info(int total, int passed, int fails){
+	change_color(13, 0);
+	printf(" %d ", total);
+	change_color(11, 0);
+	printf("tests runned.");
+	change_color(13, 0);
+	printf(" %d ", passed);
+	change_color(11, 0);
+	printf("passed.");
+	change_color(13, 0);
+	printf(" %d ", fails);
+	change_color(11, 0);
+	printf("failed.\n");
+	change_color(7, 0);
+}
 int main(int argc, char *argv[]) {
 	int passed = 0;
 	int fails = 0;
 	int total = 0;
 	bool interpreted = true;
+	bool isVerbose = strcmp(argv[argc-1], "-v") == 0 || strcmp(argv[argc-1], "--verbose") == 0;
 	if(argv[1] == NULL){
 		printf("Especifique um arquivo para testes!");
 		return 1;
 	}
-	if(argv[2] != NULL){
-		if(strcmp(argv[2], "-v") == 0 || strcmp(argv[2], "--verbose") == 0)
-			interpreted = tester(argv[1], true, &passed, &fails, &total);	
-	}else{
-		interpreted = tester(argv[1], false, &passed, &fails, &total);
-	}
 	
-	if(interpreted){
-		change_color(13, 0);
-		printf(" %d ", total);
-		change_color(11, 0);
-		printf("tests runned.");
-		change_color(13, 0);
-		printf(" %d ", passed);
-		change_color(11, 0);
-		printf("passed.");
-		change_color(13, 0);
-		printf(" %d ", fails);
-		change_color(11, 0);
-		printf("failed.");
-		change_color(7, 0);
+	int quant = (isVerbose) ? argc - 1 : argc;
+	
+	for(int i = 1; i < quant; i++){
+		interpreted = (isVerbose) 	? tester(argv[i], true, &passed, &fails, &total)
+									: tester(argv[i], false, &passed, &fails, &total);
+		if(interpreted)
+			show_info(total, passed, fails);
+			
+		total = 0;
+		fails = 0;
+		passed = 0;
 	}
 	
 	return 0;
